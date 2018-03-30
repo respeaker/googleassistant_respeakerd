7e0ef1adc62f2a02c7524f6a6d6ee25c89a4e822
KillingJacky <jacky.shaoxg@gmail.com>
2018-03-30 03:43:49 +0000

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='googleassistant_respeakerd',
-    version='0.1.0',
+    version='0.1.2',
     description=README,
     long_description=README,
     author="Jack Shao",
